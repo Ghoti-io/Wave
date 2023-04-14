7ddb82b3b31c803f8ea107f9aec94d6ed1c0e62e
Corey Pennycuff <pennycuff.c@gmail.com>
2023-04-14 00:55:18 -0500

    Add missing macros.hpp

---
+++ include/wave/macros.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Header file for declaring the Client class.
+ */
+
+#ifndef MACROS_HPP
+#define MACROS_HPP
+
+
+namespace Ghoti::Wave {
+  constexpr size_t MAXBUFFERSIZE = 4096;
+};
+
+#endif // MACROS_HPP
+
+
